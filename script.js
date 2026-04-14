7d8f06cbd63c3d401654f8d1c57a47fded766faa
Kunal Kumar <kunaloct268@gmail.com>
2026-04-14 14:44:07 +0530

    Add drag element into items(todo,progess and done)

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
+let dragElemet= null;
 
 
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(tasks => {
     tasks.addEventListener("drag", (e) => {
-
+     dragElemet=tasks;
     })
 });
 
@@ -25,8 +26,20 @@ function addDragEventsOnColumn(column){
         column.classList.add("hover-over");
     })
     column.addEventListener("dragleave",(e)=>{
-        e.preventdDfault();
+       e.preventDefault();
         column.classList.remove("hover-over");
+    })
+
+    column.addEventListener("dragover",(e)=>{
+        e.preventDefault();
+    })
+    column.addEventListener("drop",(e)=>{
+        e.preventDefault();
+
+        console.log("dropped", dragElemet,column);
+        column.appendChild(dragElemet);
+        column.classList.remove("hover-over");
+
     })
 }
 
