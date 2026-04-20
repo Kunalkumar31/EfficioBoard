2a163b78821b1f9cc933eabb50861d42fe049ce6
Kunal Kumar <kunaloct268@gmail.com>
2026-04-20 13:33:41 +0530

    Create Add task and also add styling for draggable

--- script.js
+++ script.js
@@ -48,16 +48,37 @@ addDragEventsOnColumn(done);
 
 /*Modal related logic*/
 const toggleModalButton = document.querySelector("#toggle-modal");
-const modalBg= document.querySelector(".modal .bg")
-const modal = document.querySelector(".modal")
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+const addTaskButton = document.querySelector("#add-new-task");
 
-toggleModalButton.addEventListener("click",()=>{
+
+toggleModalButton.addEventListener("click", () => {
     modal.classList.toggle("active");
 })
-modalBg.addEventListener("click",()=>{
+modalBg.addEventListener("click", () => {
     modal.classList.remove("active");
 })
 
+addTaskButton.addEventListener("click", () => {
+    const taskTittele = document.querySelector("#task-title-input").value
+    const taskDesc = document.querySelector("#task-desc-input").value
+
+    const div = document.createElement("div");
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+
+    div.innerHTML = `
+    <h2>${taskTittele}</h2>
+    <p>${taskDesc}</p>
+    <button>Delete</button>
+    `
+    todo.appendChild(div);
+    div.addEventListener("drag",(e)=>{
+        dragElemet=div;
+    })
+    modal.classList.remove("active");
+})
 
 
 
